2dc7685a9b812899b022983195651939364c38b8
Daniel Clover <danil.word@mail.ru>
2023-04-21 18:47:06 +0500

    Добавьте файлы проекта.

---
+++ Handbook/Data/HandbookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Handbook.Models;
+
+namespace Handbook.Data
+{
+    public class HandbookContext : DbContext
+    {
+        public HandbookContext (DbContextOptions<HandbookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Handbook.Models.Employees> Employees { get; set; } = default!;
+    }
+}
---
+++ Handbook/Models/Employees.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Handbook.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Full name")]
+        public string FullName { get; set; }
+        public string Department { get; set; }
+
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Cabinet number")]
+        public string CabinetNumber { get; set; }
+
+    }
+}
